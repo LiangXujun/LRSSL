65a4ff4c7cb611887dffde782a757e2b116c0f31
LiangXujun <lichare@qq.com>
2016-07-21 08:36:52 +0800

    update

--- LRSSL.R
+++ LRSSL.R
@@ -21,3 +21,8 @@ S1 <- X1%*%t(X1)/sqrt(rowSums(X1)%*%t(rowSums(X1)))
 S2 <- X2%*%t(X2)/sqrt(rowSums(X1)%*%t(rowSums(X1)))
 S3 <- X3%*%t(X3)/sqrt(rowSums(X3)%*%t(rowSums(X3)))
 
+S1 <- S1 - diag(S1)
+S2 <- S2 - diag(S2)
+S3 <- S3 - diag(S3)
+
+ind.knn1 <- sort
